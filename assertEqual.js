6d72d36d6d46fb731ca13349808e162ad4619461
Jeandre Visser <jeandrev1414@gmail.com>
2022-06-27 22:11:26 +0000

    Created assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅Assertion Passed: ' + actual + " === " + expected);
+  } else {
+    console.log('❌Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('My name is Jeandre', 'My name is Jeandre');
+assertEqual('It is a nice day', 'It is a Nice day');
+assertEqual(42, 42);
+assertEqual(1, 10);
