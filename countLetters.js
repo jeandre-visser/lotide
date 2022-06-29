c83cd76de98b1bb976c1471b578e2f6a27a926d3
Jeandre Visser <jeandrev1414@gmail.com>
2022-06-29 23:54:00 +0000

    Implemented test cases

--- countLetters.js
+++ countLetters.js
@@ -21,5 +21,12 @@ const countLetters = function(str) {
 };
 
 // TEST CASES
-console.log(countLetters("LHL"));
+console.log(countLetters("Lighthouse Labs Rocks"));
 console.log(countLetters("Bitcoin"));
+
+const wolf = "Jordan Belfort";
+let result1 = countLetters(wolf);
+assertEqual(result1["J"], 1);
+assertEqual(result1["o"], 2);
+assertEqual(result1["y"], undefined);
+assertEqual(result1["t"], 1);
