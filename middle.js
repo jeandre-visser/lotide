93b9605605d09b9041f9faf4f45ff51fc2d55625
Jeandre Visser <jeandrev1414@gmail.com>
2022-06-29 21:45:22 +0000

    Added middle function

---
+++ middle.js
@@ -0,0 +1,36 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const middle = function(arr) {
+
+  const midNum = Math.floor(arr.length / 2);
+
+  if (arr.length <= 2) {
+    return [];
+
+  } else if (arr.length % 2 === 0) {
+    return [arr[midNum - 1], arr[midNum]]
+
+  } else {
+      return [arr[midNum]];
+  }
+}
+
+console.log(assertArraysEqual(middle([1, 2, 3, 4])))
