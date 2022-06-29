1b35485f649df2a1df9453fd98757b3db8308c90
Jeandre Visser <jeandrev1414@gmail.com>
2022-06-29 02:49:39 +0000

    Implemented flattening of nested arrays

---
+++ flatten.js
@@ -0,0 +1,35 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const flatten = function(arr) {
+  let flattenedArr = [];
+  for (let element of arr) {
+    if (Array.isArray(element)) {
+      for (let nestedElement of element) {
+        flattenedArr.push(nestedElement);
+      }
+    } else {
+      flattenedArr.push(element);
+    }
+  }
+  return flattenedArr;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
