3ff40859847ee667a6ad1b1790707d0414961bf7
Jeandre Visser <jeandrev1414@gmail.com>
2022-06-30 00:20:50 +0000

    Added if loops to skip spaces and to add the letter/index location key value pair to the results object

--- letterPositions.js
+++ letterPositions.js
@@ -19,18 +19,14 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 
-const letterPositions = sentence => {
+const letterPositions = function(sentence) {
   const results = {};
-  // loop through string
+  
+  
   for (let i = 0; i < sentence.length; i++) {
-    // if statement to skip spaces
-    if (sentence[i] !== ' ') {
-      // add key value to object
+    if (sentence[i] !== " ") {
       if (results[sentence[i]]) {
-        // find index in sentence and push to results arr
-        results[sentence[i]].push(i);
-      } else {
-        results[sentence[i]] = [i];
+        
       }
     }
   }
@@ -38,11 +34,3 @@ const letterPositions = sentence => {
 };
 
 
-
-// TEST CODES
-console.log(letterPositions("hello"))
-
-assertArraysEqual(letterPositions("hello").h, [0]);
-assertArraysEqual(letterPositions("hello").e, [1]);
-assertArraysEqual(letterPositions("hello").l, [2, 3]);
-assertArraysEqual(letterPositions("hello").o, [4]);
