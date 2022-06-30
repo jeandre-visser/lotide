f992774f7900955ae212f1bb79eb27ed9324b1d0
Jeandre Visser <jeandrev1414@gmail.com>
2022-06-30 03:22:18 +0000

    added test cases

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -24,5 +24,10 @@ const assertObjectsEqual = function(actual, expected) {
   if (eqObjects(actual, expected)) {
     return console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`)
   }
-  return `❌Assertion Passed: ${inspect(actual)} !== ${inspect(expected)}`
+  return console.log(`❌Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`)
 };
+
+// TEST CASES
+assertObjectsEqual({a: 1, b: 2}, {b: 2, a: 1});
+assertObjectsEqual({a: 1, b: 2, c: 3}, {b: 2, a: 1, c: 3, d: 4});
+assertObjectsEqual({a: 'hi', b: 'why'}, {b: 'hi', a: 'why'});
