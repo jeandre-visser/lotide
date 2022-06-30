e3ea781b060efb0294961adafb0a06ae5a5df562
Jeandre Visser <jeandrev1414@gmail.com>
2022-06-30 18:06:46 +0000

    Added comments

--- countOnly.js
+++ countOnly.js
@@ -11,14 +11,12 @@ const assertEqual = function(actual, expected) {
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-
+  // loop through each item in allItems
   for (let item of allItems) {
-
     if (itemsToCount[item]) {
-
+      // increase count of each instance of item
       if (results[item]) {
         results[item] += 1;
-
       } else {
         results[item] = 1;
       }
