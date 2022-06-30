bf03dab81bbe9f94f51ade5ae20b1cc825e2a8fd
Jeandre Visser <jeandrev1414@gmail.com>
2022-06-30 03:35:24 +0000

    Removed else from if statment from assertEqual

--- head.js
+++ head.js
@@ -1,17 +1,13 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+    return console.log(`✅Assertion Passed: ${actual} === ${expected}`);
   }
+  return console.log(`❌Assertion Failed: ${actual} !== ${expected}`); 
 };
 
 const head = function(arr) {
   return arr[0];
 };
-
-
-
 
 
 // TEST CODE
