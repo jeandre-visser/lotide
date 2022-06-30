918b541c42c9da1f2e126e4b6ea94576798ad241
Jeandre Visser <jeandrev1414@gmail.com>
2022-06-30 17:50:08 +0000

    Added comments

--- eqObjects.js
+++ eqObjects.js
@@ -18,22 +18,21 @@ const eqArrays = function(arr1, arr2) {
 };
 
 const eqObjects = function(obj1, obj2) {
+  // obtain key arrays using Object.keys
   let keyArr1 = Object.keys(obj1);
   let keyArr2 = Object.keys(obj2);
+  // return false if the key arrays lengths don't match
   if (keyArr1.length !== keyArr2.length) {
     return false;
   }
   for (let key of keyArr1) {
-
     if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
       return eqArrays(obj1[key], obj2[key]);
     }
-
     if (obj1[key] !== obj2[key]) {
       return false;
     }
   }
-
   return true;
 };
 
