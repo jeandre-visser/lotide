9fcce0696eafd500660675e9839336e0c493c858
Jeandre Visser <jeandrev1414@gmail.com>
2022-06-30 03:34:53 +0000

    Removed else from if statment from assertArraysEqual

--- flatten.js
+++ flatten.js
@@ -12,10 +12,9 @@ const eqArrays = function(arr1, arr2) {
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log(`✅Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌Assertion Failed: ${arr1} !== ${arr2}`);
+    return console.log(`✅Assertion Passed: ${arr1} === ${arr2}`);
   }
+  return console.log(`❌Assertion Failed: ${arr1} !== ${arr2}`);
 };
 
 const flatten = function(arr) {
