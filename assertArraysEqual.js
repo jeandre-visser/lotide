f8eb974a0df6977cb85468a5f26ccd185c6e44a5
Jeandre Visser <jeandrev1414@gmail.com>
2022-06-30 03:33:08 +0000

    Removed else from if statment from assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -12,8 +12,7 @@ const eqArrays = function(arr1, arr2) {
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log(`✅Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌Assertion Failed: ${arr1} !== ${arr2}`);
+    return console.log(`✅Assertion Passed: ${arr1} === ${arr2}`);
   }
+  return console.log(`❌Assertion Failed: ${arr1} !== ${arr2}`);
 };
