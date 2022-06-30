cb9b34d37674cdc842f311f0419308c4ad7aaf2c
Jeandre Visser <jeandrev1414@gmail.com>
2022-06-30 00:24:08 +0000

    Added test codes

--- letterPositions.js
+++ letterPositions.js
@@ -36,3 +36,10 @@ const letterPositions = function(sentence) {
 };
 
 
+// TEST CODES
+console.log(letterPositions("hello"));
+
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
