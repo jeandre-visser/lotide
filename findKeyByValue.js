59451180e30c7401168dc52203ca36620ab11792
Jeandre Visser <jeandrev1414@gmail.com>
2022-06-30 01:14:10 +0000

    Added test cases

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -18,4 +18,32 @@ const findKeyByValue = function(object, value) {
 };
 
 
+// TEST CASES
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(['one']), 'one');
+assertEqual(head([]), undefined);
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const bestTeamsBySport = {
+  hockey: "Colorado Avalanche",
+  basketball: "LA Lakers",
+  rugby: "South African Springboks"
+};
+
+assertEqual(findKeyByValue(bestTeamsBySport, "Edmonton Oilers"), undefined);
+assertEqual(findKeyByValue(bestTeamsBySport, "Colorado Avalanche"), "hockey");
+assertEqual(findKeyByValue(bestTeamsBySport, "South African Springboks"), "rugby");
+
+
 
