76440f288a7e51e1f317b83c25e64d2741da3756
Jeandre Visser <jeandrev1414@gmail.com>
2022-06-30 00:26:01 +0000

    Test code identified error, fixed errors

--- letterPositions.js
+++ letterPositions.js
@@ -26,7 +26,7 @@ const letterPositions = function(sentence) {
   for (let i = 0; i < sentence.length; i++) {
     if (sentence[i] !== " ") {
       if (results[sentence[i]]) {
-        results[sentence[i]].push[i];
+        results[sentence[i]].push(i);
       } else {
         results[sentence[i]] = [i];
       }
