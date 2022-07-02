357720c008e31efc283ac5cb75c7544321efa737
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-02 01:38:36 +0000

    Added comments

--- middle.js
+++ middle.js
@@ -18,15 +18,17 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 const middle = function(arr) {
-
+  // determine middle of array, rounding down to nearest integer
   const midNum = Math.floor(arr.length / 2);
-
+  // return nothing if array has two or less numbers in it
   if (arr.length <= 2) {
     return [];
   }
+  // return the middle two numbers if the array is even
   if (arr.length % 2 === 0) {
     return [arr[midNum - 1], arr[midNum]]
   }
+  // otherwise return the middle number if array is odd
     return [arr[midNum]];
 }
 
