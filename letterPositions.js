ca39c5732ea16b63ee9ff4a85b71bcb278e91f51
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-02 01:24:44 +0000

    Added comments explaining code

--- letterPositions.js
+++ letterPositions.js
@@ -20,11 +20,11 @@ const assertArraysEqual = function(arr1, arr2) {
 
 const letterPositions = function(sentence) {
   const results = {};
-  
+  // splits up sentence into letters and each letter is input into an array
   sentence.split("").forEach(letter => {
     results[letter] = []
   });
-
+  // for loop that determines at which index each letter occurs, and pushes that index position into array
   for (let i = 0; i < sentence.length; i++) {
     results[sentence[i]].push(i);
   }
