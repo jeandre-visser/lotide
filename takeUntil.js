62be29d74b7e55f8858a6630d50915c70224f954
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-03 23:22:33 +0000

    Imported assertArraysEqual and eqArrays

--- takeUntil.js
+++ takeUntil.js
@@ -23,25 +23,10 @@ const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
 // eqArrays function
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 // assertEqualsArray function
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    return console.log(`✅Assertion Passed: ${arr1} === ${arr2}`);
-  }
-  return console.log(`❌Assertion Failed: ${arr1} !== ${arr2}`);
-};
+const assertArraysEqual = require("./assertArraysEqual");
 
 // TEST CODE
 
