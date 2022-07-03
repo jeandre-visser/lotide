3489e4c222f88a85f18d3e6953c7baf5698189f6
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-03 23:14:00 +0000

    required assertEqual and exported eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -1,21 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  }
-  return console.log(`❌Assertion Failed: ${actual} !== ${expected}`); 
-};
+const assertEqual = require("./assertEqual");
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+
 
 const eqObjects = function(obj1, obj2) {
   // obtain key arrays using Object.keys
@@ -50,3 +35,10 @@ console.log(eqObjects(cd, dc)); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 console.log(eqObjects(cd, cd2)); // => false
+
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }))// => true
+
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => false
+
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })) // => false
+
