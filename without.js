3b5115c1e3aab71ee53995404625a0f8d1a35e57
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-03 23:24:03 +0000

    Imported assertArrraysEqual and eqArrays

--- without.js
+++ without.js
@@ -1,21 +1,6 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    return console.log(`✅Assertion Passed: ${arr1} === ${arr2}`);
-  }
-  return console.log(`❌Assertion Failed: ${arr1} !== ${arr2}`);
-};
+const assertArraysEqual = require("./assertArraysEqual");
 
 const without = function(src, itemsToRemove) {
   let newArr = [];
