73f527139f933ef2e05a406bf7abde78032822c3
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-04 20:17:32 +0000

    Added test folder for test cases of lotide functions

---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('My name is Jeandre', 'My name is Jeandre');
+assertEqual('It is a nice day', 'It is a Nice day');
+assertEqual(42, 42);
+assertEqual(1, 10);
+
