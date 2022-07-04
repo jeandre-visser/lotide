ed98572984c5b5d7d99675e40e6e6e29d169cc63
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-04 21:10:36 +0000

    Refactored tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -12,30 +12,3 @@ describe("#tail", () => {
   });
 
 });
-
-
-
-
-
-
-// const tail = require('../tail');
-// const assertEqual = require('../assertEqual');
-
-// // TEST CASE 1
-// const testOne = ["Hello", "Lighthouse", "Labs"];
-// console.log(tail(testOne));
-// assertEqual(testOne.length, 3);
-// assertEqual(tail(testOne).length, 2);
-
-// // TEST CASE 2
-// let testTwo = ["Lighthouse"];
-// console.log(tail(testTwo));
-// assertEqual(testTwo.length, 1);
-// assertEqual(tail(testTwo).length, 0);
-
-
-// // TEST CASE 3
-// let testThree = [ ];
-// console.log(tail(testThree));
-// assertEqual(testThree.length, 0);
-// assertEqual(tail(testThree).length, 0);
