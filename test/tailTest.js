7c3e42087a10652538540884d63afaf3dd6b8a27
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-04 21:09:41 +0000

    Refactored tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,21 +1,41 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
-
-// TEST CASE 1
-const testOne = ["Hello", "Lighthouse", "Labs"];
-console.log(tail(testOne));
-assertEqual(testOne.length, 3);
-assertEqual(tail(testOne).length, 2);
-
-// TEST CASE 2
-let testTwo = ["Lighthouse"];
-console.log(tail(testTwo));
-assertEqual(testTwo.length, 1);
-assertEqual(tail(testTwo).length, 0);
 
 
-// TEST CASE 3
-let testThree = [ ];
-console.log(tail(testThree));
-assertEqual(testThree.length, 0);
-assertEqual(tail(testThree).length, 0);
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns [6, 7, 8] for [5, 6, 7, 8]", () => {
+    assert.deepEqual(tail([5, 6, 7, 8]), [6, 7, 8]); 
+  });
+
+});
+
+
+
+
+
+
+// const tail = require('../tail');
+// const assertEqual = require('../assertEqual');
+
+// // TEST CASE 1
+// const testOne = ["Hello", "Lighthouse", "Labs"];
+// console.log(tail(testOne));
+// assertEqual(testOne.length, 3);
+// assertEqual(tail(testOne).length, 2);
+
+// // TEST CASE 2
+// let testTwo = ["Lighthouse"];
+// console.log(tail(testTwo));
+// assertEqual(testTwo.length, 1);
+// assertEqual(tail(testTwo).length, 0);
+
+
+// // TEST CASE 3
+// let testThree = [ ];
+// console.log(tail(testThree));
+// assertEqual(testThree.length, 0);
+// assertEqual(tail(testThree).length, 0);
