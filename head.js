6749d8ddf53cd108f7c453afce3ad88f48333e2f
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-04 20:17:08 +0000

    Refactored code

--- head.js
+++ head.js
@@ -5,8 +5,4 @@ const head = function(arr) {
 };
 
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(['one']), 'one');
-assertEqual(head([]), undefined);
+module.exports = head;
