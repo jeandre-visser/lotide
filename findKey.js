2708e65dcfbdaa2aaee89d177b618f224244a2f5
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-05 00:28:51 +0000

    Removed test cases

--- findKey.js
+++ findKey.js
@@ -13,37 +13,6 @@ const findKey = (object, callback) => {
   return results;
 }
 
-// TEST CASES
-let test1 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+module.exports = findKey;
 
-assertEqual(test1, 'noma')
-
-let test2 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 0) // => "undefined"
-
-assertEqual(test2, undefined)
-
-let test3 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 3) // => "Akaleri"
-
-assertEqual(test3, 'Akaleri')
 
