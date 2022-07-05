de58e73980519141ee492d3b1fdf2e0d086a3f03
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-05 00:22:22 +0000

    Refactored

--- countLetters.js
+++ countLetters.js
@@ -1,4 +1,3 @@
-const assertEqual = require('./assertEqual');
 
 const countLetters = function(str) {
   let letterCounter = {};
@@ -16,16 +15,4 @@ const countLetters = function(str) {
   return letterCounter;
 };
 
-// TEST CASES
-console.log(countLetters("Lighthouse Labs Rocks"));
-console.log(countLetters("Bitcoin"));
-
-const wolf = "Jordan Belfort";
-let result1 = countLetters(wolf);
-assertEqual(result1["J"], 1);
-assertEqual(result1["o"], 2);
-assertEqual(result1["y"], undefined);
-assertEqual(result1["t"], 1);
-
 module.exports = countLetters;
-
