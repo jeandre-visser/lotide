235a48f001cfab613a59b96a64465004ffcaad63
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-05 00:19:21 +0000

    Removed test cases and added to test folder

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -14,27 +14,8 @@ const findKeyByValue = function(object, value) {
   }
 };
 
+module.exports = findKeyByValue;
 
-// TEST CASES
-
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-
-const bestTeamsBySport = {
-  hockey: "Colorado Avalanche",
-  basketball: "LA Lakers",
-  rugby: "South African Springboks"
-};
-
-assertEqual(findKeyByValue(bestTeamsBySport, "Edmonton Oilers"), undefined);
-assertEqual(findKeyByValue(bestTeamsBySport, "Colorado Avalanche"), "hockey");
-assertEqual(findKeyByValue(bestTeamsBySport, "South African Springboks"), "rugby");
 
 
 
