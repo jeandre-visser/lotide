eb9fd9c0c673f434a58b996a4cb71deb317547ba
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-05 00:34:02 +0000

    Refactored

--- map.js
+++ map.js
@@ -1,5 +1,3 @@
-const words = ["ground", "control", "to", "major", "tom"];
-
 
 const map = function(array, callback) {
   const results = [];
@@ -11,24 +9,6 @@ const map = function(array, callback) {
   return results;
 }
 
-// calls upon our map function
-const results1 = map(words, word => word[0]);
-console.log(results1);
+module.exports = map;
 
-// eqArrays function
-const eqArrays = require("./eqArrays");
 
-// assertArraysEqual function
-const assertArraysEqual = require("./assertArraysEqual");
-// TEST CODE
-assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]); // PASS
-
-// New test array
-const sports = ['hockey', 'golf', 'rugby', 'tennis'];
-
-// Tests on sports array
-const test1 = map(sports, sport => sport[1]);
-const test2 = map(sports, sport => sport[2]);
-
-assertArraysEqual(test1, [ "o", "o", "u", "e" ]); // PASS
-assertArraysEqual(test2, [ "c", "l", "g", "n" ]); // PASS
