d94042d49d37860dd41a402cf54b89ae02cd7308
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-05 00:38:33 +0000

    Removed test cases

--- without.js
+++ without.js
@@ -1,7 +1,3 @@
-const eqArrays = require("./eqArrays");
-
-const assertArraysEqual = require("./assertArraysEqual");
-
 const without = function(src, itemsToRemove) {
   let newArr = [];
   // loop through each item of array
@@ -14,12 +10,7 @@ const without = function(src, itemsToRemove) {
   return newArr;
 };
 
+module.exports = without;
 
-console.log(without([1, 2, 3], [1])); // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
