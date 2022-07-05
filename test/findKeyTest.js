b1485dc054e4ce5c1a18f8a85f381649ab0409b5
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-05 00:30:06 +0000

    Added test cases

---
+++ test/findKeyTest.js
@@ -0,0 +1,37 @@
+const findKey = require('../findKey');
+const assertEqual = require('../assertEqual');
+
+
+// TEST CASES
+let test1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(test1, 'noma')
+
+let test2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 0) // => "undefined"
+
+assertEqual(test2, undefined)
+
+let test3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3) // => "Akaleri"
+
+assertEqual(test3, 'Akaleri')
